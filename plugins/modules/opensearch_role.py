442063de83e5cffb2fc609bb4db808c44b663078
Castor Sky <csky57@gmail.com>
2026-02-09 04:46:47 +0000

    refactor: update role module for httpapi methods

--- plugins/modules/opensearch_role.py
+++ plugins/modules/opensearch_role.py
@@ -30,29 +30,40 @@ EXAMPLES = """
 
 __metaclass__ = type  # pylint: disable=C0103
 
-from ansible_collections.castorsky.opensearch.plugins.module_utils.opensearch import OpenSearchModule
+from ansible_collections.castorsky.opensearch.plugins.module_utils.opensearch import (
+    OpenSearchModule,
+)
 
 
 def main() -> None:
     module_argument_spec = dict(
-        name=dict(type='str', required=True),
-        cluster_permissions=dict(type='list', default=[]),
-        index_permissions=dict(type='list', default=[]),
-        tenant_permissions=dict(type='list', default=[]),
-        state=dict(type='str', choices=['present', 'absent'], default='present'),
-        description=dict(type='str', default=''),
+        name=dict(type="str", required=True),
+        cluster_permissions=dict(type="list", default=[]),
+        index_permissions=dict(type="list", default=[]),
+        tenant_permissions=dict(type="list", default=[]),
+        state=dict(type="str", choices=["present", "absent"], default="present"),
+        description=dict(type="str", default=""),
     )
 
     module = OpenSearchModule(
         argument_spec=module_argument_spec,
+        supports_check_mode=True,
+        api_prefix="/_plugins/_security/api/roles/",
     )
 
-    mandatory_params = ['cluster_permissions', 'index_permissions', 'tenant_permissions', 'description']
-    module.default_sequence(api_group='security', object_type='role', object_params=mandatory_params)
+    # Parameters that are used in OpenSearch API request body.
+    role_parameters = {
+        "cluster_permissions": module.params["cluster_permissions"],
+        "index_permissions": module.params["index_permissions"],
+        "tenant_permissions": module.params["tenant_permissions"],
+        "description": module.params["description"],
+    }
 
-    result = {'changed': module.changed, 'content': module.result}
+    changed_flag, module_result = module.security_crud_sequence(role_parameters)
+
+    result = {"changed": changed_flag, "content": module_result}
     module.exit_json(**result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
